64fc97979cd08704ef347a22106a964e4f9637e5
jackytsheng <jackytsheng@gmail.com>
2021-01-10 00:34:11 +1100

    added get method

--- Commander/Controllers/CommandController.cs
+++ Commander/Controllers/CommandController.cs
@@ -1,15 +1,30 @@
-using System.Collections;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Commander.Models;
+using Commander.Data;
 
 namespace Commander.Controllers
 {
-  [Route("api/[controller]")]
+  [Route("api/commands")]
   [ApiController]
   public class CommandController : ControllerBase
   {
+    private readonly MockcommanderRepo _repository = new MockcommanderRepo();
+    //Get api/commands
+    [HttpGet]
     public ActionResult<IEnumerable<Command>> GetAllCommands()
     {
-      
+      var commandItems = _repository.GetAppCommands();
+
+      return Ok(commandItems);
+    }
+    //Get api/comands/{id}
+    [HttpGet("{id}")]
+    public ActionResult <Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+
+      return Ok(id);
     }
   }
 }
